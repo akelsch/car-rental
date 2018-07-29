e807ef4e1be3f00437bc39afe80ff3836e63183a
Arthur Kelsch <akelsch@users.noreply.github.com>
2018-07-29 03:02:21 +0200

    First attempt at creating a three layer structure for Car objects

---
+++ src/main/java/de/htwsaar/prog3/carrental/model/Car.java
@@ -0,0 +1,77 @@
+package de.htwsaar.prog3.carrental.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Car model for Hibernate.
+ *
+ * @author Arthur Kelsch
+ */
+@Entity
+@Table
+@Data
+public class Car {
+    public Car() {
+    }
+
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id;
+
+    @Column(name = "daily_rate", nullable = false)
+    private int dailyRate;
+
+    @Column(name = "door_count", nullable = false)
+    private int doorCount;
+
+    @Column(name = "driven_distance", nullable = false)
+    private int drivenDistance;
+
+    @Column(nullable = false)
+    private int horsepower;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(name = "construction_year", nullable = false)
+    private String constructionYear;
+
+    @Column
+    private String defects;
+
+    @Column
+    private String equipment;
+
+    @Column(nullable = false)
+    private String fuel;
+
+    @Column(nullable = false)
+    private String gearbox;
+
+    @Column(name = "license_number", nullable = false)
+    private String licenseNumber;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(name = "next_inspection", nullable = false)
+    private String nextInspection;
+
+    @Column(name = "parking_lot", nullable = false, unique = true)
+    private String parkingLot;
+
+    @Column(nullable = false)
+    private String tires;
+
+    @Column(nullable = false)
+    private String vin;
+}
---
+++ src/test/java/de/htwsaar/prog3/carrental/CarServiceTest.java
@@ -0,0 +1,43 @@
+package de.htwsaar.prog3.carrental;
+
+import de.htwsaar.prog3.carrental.model.Car;
+import de.htwsaar.prog3.carrental.service.CarService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CarServiceTest {
+    private CarService carService;
+
+    @BeforeEach
+    void setUp() {
+        this.carService = new CarService();
+    }
+
+    @Test
+    void testCreateAndRead() {
+        Car expectedCar = new Car();
+        expectedCar.setDailyRate(120);
+        expectedCar.setDoorCount(5);
+        expectedCar.setDrivenDistance(25000);
+        expectedCar.setHorsepower(303);
+        expectedCar.setBrand("BMW");
+        expectedCar.setCategory("Sedan");
+        expectedCar.setColor("Black");
+        expectedCar.setConstructionYear("2016");
+        expectedCar.setFuel("Gasoline");
+        expectedCar.setGearbox("Manual");
+        expectedCar.setLicenseNumber("HD GG 1234");
+        expectedCar.setModel("335i");
+        expectedCar.setNextInspection("04-2010");
+        expectedCar.setParkingLot("2A");
+        expectedCar.setTires("Summer Tires");
+        expectedCar.setVin("1FTJX35G4RKA95915");
+
+        carService.persist(expectedCar);
+        Car actualCar = carService.findById(1L);
+
+        assertEquals(expectedCar, actualCar);
+    }
+}
