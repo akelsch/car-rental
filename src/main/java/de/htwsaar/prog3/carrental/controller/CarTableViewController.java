65ec1828a6be2efe9710a8c407e179895e4bc367
Lukas Raubuch <35454706+lraubuch@users.noreply.github.com>
2018-08-29 15:33:09 +0200

    Implemented the displaying of cars and completed deletion functionality

--- src/main/java/de/htwsaar/prog3/carrental/controller/CarTableViewController.java
+++ src/main/java/de/htwsaar/prog3/carrental/controller/CarTableViewController.java
@@ -1,29 +1,74 @@
 package de.htwsaar.prog3.carrental.controller;
 
+import java.net.URL;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 import de.htwsaar.prog3.carrental.i18n.I18nComponentsUtil;
 import de.htwsaar.prog3.carrental.model.Car;
 import de.htwsaar.prog3.carrental.service.CarService;
 import de.htwsaar.prog3.carrental.util.GUIDialogUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * This is the Controller for the Main View of the Carrental Application
  * 
  * @author Lukas Raubuch
  */
-public class CarTableViewController {
+public class CarTableViewController implements Initializable {
 
-	private CarService service;
+	private CarService service = new CarService();
+	private ObservableList<Car> data = FXCollections.observableArrayList(service.findAll());
 
 	@FXML
 	private TableView<Car> carTableView;
+	// TableColumns to associate Data with columns
+	@FXML
+	private TableColumn<Car, Integer> id;
+	@FXML
+	private TableColumn<Car, String> brand;
+	@FXML
+	private TableColumn<Car, String> category;
+	@FXML
+	private TableColumn<Car, String> color;
+	@FXML
+	private TableColumn<Car, String> constructionYear;
+	@FXML
+	private TableColumn<Car, Integer> dailyRate;
+	@FXML
+	private TableColumn<Car, String> defects;
+	@FXML
+	private TableColumn<Car, Integer> doorCount;
+	@FXML
+	private TableColumn<Car, Integer> drivenDistance;
+	@FXML
+	private TableColumn<Car, String> fuel;
+	@FXML
+	private TableColumn<Car, String> gearbox;
+	@FXML
+	private TableColumn<Car, Integer> horsepower;
+	@FXML
+	private TableColumn<Car, String> licenseNumber;
+	@FXML
+	private TableColumn<Car, String> model;
+	@FXML
+	private TableColumn<Car, String> nextInspection;
+	@FXML
+	private TableColumn<Car, String> parkingLot;
+	@FXML
+	private TableColumn<Car, String> tires;
+	@FXML
+	private TableColumn<Car, String> vin;
 
 	@FXML
 	private Button editSelectedCarButton;
@@ -78,11 +123,41 @@ public class CarTableViewController {
 			informationDialog.show();
 			return;
 		}
-		Alert confirmationDialog = GUIDialogUtil.createConfirmationDialog(I18nComponentsUtil.getConfirmationDialogHeaderDelete());
+		Alert confirmationDialog = GUIDialogUtil
+				.createConfirmationDialog(I18nComponentsUtil.getConfirmationDialogHeaderDelete());
 		Optional<ButtonType> result = confirmationDialog.showAndWait();
 		if (result.get() == ButtonType.OK) {
 			service = new CarService();
 			service.delete(toDelete);
+			data.remove(toDelete);
 		}
+	}
+
+	@Override
+	/**
+	 * Initialize the CarTableView with data from the database
+	 */
+	public void initialize(URL location, ResourceBundle resources) {
+		id.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		brand.setCellValueFactory(new PropertyValueFactory<>("Brand"));
+		category.setCellValueFactory(new PropertyValueFactory<>("Category"));
+		color.setCellValueFactory(new PropertyValueFactory<>("Color"));
+		constructionYear.setCellValueFactory(new PropertyValueFactory<>("ConstructionYear"));
+		dailyRate.setCellValueFactory(new PropertyValueFactory<>("DailyRate"));
+		defects.setCellValueFactory(new PropertyValueFactory<>("Defects"));
+		doorCount.setCellValueFactory(new PropertyValueFactory<>("DoorCount"));
+		drivenDistance.setCellValueFactory(new PropertyValueFactory<>("DrivenDistance"));
+		fuel.setCellValueFactory(new PropertyValueFactory<>("Fuel"));
+		gearbox.setCellValueFactory(new PropertyValueFactory<>("Gearbox"));
+		horsepower.setCellValueFactory(new PropertyValueFactory<>("Horsepower"));
+		licenseNumber.setCellValueFactory(new PropertyValueFactory<>("LicenseNumber"));
+		model.setCellValueFactory(new PropertyValueFactory<>("Model"));
+		nextInspection.setCellValueFactory(new PropertyValueFactory<>("NextInspection"));
+		parkingLot.setCellValueFactory(new PropertyValueFactory<>("ParkingLot"));
+		tires.setCellValueFactory(new PropertyValueFactory<>("Tires"));
+		vin.setCellValueFactory(new PropertyValueFactory<>("Vin"));
+
+		carTableView.setItems(data);
+
 	}
 }
--- src/main/java/de/htwsaar/prog3/carrental/i18n/I18nComponentsUtil.java
+++ src/main/java/de/htwsaar/prog3/carrental/i18n/I18nComponentsUtil.java
@@ -8,32 +8,32 @@ import java.util.ResourceBundle;
  * @author Lukas Raubuch
  */
 public class I18nComponentsUtil {
-    /**
-     * Definition of all available Strings in components_*.properties.
-     */
-    private static final String STAGE_TITLE = "stage-title";
-    private static final String DIALOG_CONFIRMATION_TITLE = "dialog-confirmation-title";
+	/**
+	 * Definition of all available Strings in components_*.properties.
+	 */
+	private static final String STAGE_TITLE = "stage-title";
+	private static final String DIALOG_CONFIRMATION_TITLE = "dialog-confirmation-title";
 	private static final String DIALOG_INFORMATION_TITLE = "dialog-information-title";
 	private static final String DIALOG_CONFIRMATION_DELETE_CAR_HEADER = "dialog-confirmation-header-delete";
 	private static final String DIALOG_INFORMATION_NO_CAR_SELECTED_HEADER = "dialog-information-header-no-car-selected";
 
-    		
-    private static ResourceBundle resourceBundle;
+	private static ResourceBundle resourceBundle;
 
-    /**
-     * Initialization of the ResourceBundle.
-     */
-    static {
-        resourceBundle = I18nUtil.getResourceBundleComponents();
-    }
+	/**
+	 * Initialization of the ResourceBundle.
+	 */
+	static {
+		resourceBundle = I18nUtil.getResourceBundleComponents();
+	}
 
-    /**
-     * Returns the internationalized String of the key defined in STAGE_TITLE.
-     * @return the internationalized String
-     */
-    public static String getStageTitleString() {
-        return resourceBundle.getString(STAGE_TITLE);
-    }
+	/**
+	 * Returns the internationalized String of the key defined in STAGE_TITLE.
+	 * 
+	 * @return the internationalized String
+	 */
+	public static String getStageTitleString() {
+		return resourceBundle.getString(STAGE_TITLE);
+	}
 
 	public static String getConfirmationDialogHeaderDelete() {
 		return resourceBundle.getString(DIALOG_CONFIRMATION_DELETE_CAR_HEADER);
