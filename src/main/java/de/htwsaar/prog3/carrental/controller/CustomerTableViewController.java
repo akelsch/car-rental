913e0ea5a31a15bced6dec5202f83920d1ecba3c
jthewes <35758939+jthewes@users.noreply.github.com>
2018-09-01 22:50:23 +0200

    CustomerTableView : New and Edit Button working now

--- src/main/java/de/htwsaar/prog3/carrental/controller/CustomerTableViewController.java
+++ src/main/java/de/htwsaar/prog3/carrental/controller/CustomerTableViewController.java
@@ -3,7 +3,8 @@ package de.htwsaar.prog3.carrental.controller;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
-
+import de.htwsaar.prog3.carrental.gui.EditCustomerView;
+import de.htwsaar.prog3.carrental.gui.NewCustomerView;
 import de.htwsaar.prog3.carrental.model.Customer;
 import de.htwsaar.prog3.carrental.service.CustomerService;
 import de.htwsaar.prog3.carrental.util.GUIDialogUtil;
@@ -20,102 +21,120 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * Controller for CustomerTableView
- * @author Lukas Raubuch
+ * 
+ * @author Lukas Raubuch, Jens Thewes
  */
 public class CustomerTableViewController extends TableViewController {
 
-	private CustomerService service = new CustomerService();
-	private ObservableList<Customer> customers = FXCollections.observableArrayList(service.findAll());
+    private CustomerService service = new CustomerService();
+    private ObservableList<Customer> customers =
+            FXCollections.observableArrayList(service.findAll());
 
-	@FXML
-	private TableView<Customer> customerTableView;
-	// TableColumns to associate data with columns
-	@FXML
-	private TableColumn<Customer, Integer> id;
-	@FXML
-	private TableColumn<Customer, String> dateOfBirth;
-	@FXML
-	private TableColumn<Customer, String> driverLicenseId;
-	@FXML
-	private TableColumn<Customer, String> emailAddress;
-	@FXML
-	private TableColumn<Customer, String> firstName;
-	@FXML
-	private TableColumn<Customer, Integer> houseNumber;
-	@FXML
-	private TableColumn<Customer, String> lastName;
-	@FXML
-	private TableColumn<Customer, String> phoneNumber;
-	@FXML
-	private TableColumn<Customer, String> street;
-	@FXML
-	private TableColumn<Customer, String> city;
-	@FXML
-	private TableColumn<Customer, String> idNumber;
-	@FXML
-	private TableColumn<Customer, String> zipCode;
-	
-	@FXML
-	private Button buttonCreateNewCustomer;
+    @FXML
+    private TableView<Customer> customerTableView;
+    // TableColumns to associate data with columns
+    @FXML
+    private TableColumn<Customer, Integer> id;
+    @FXML
+    private TableColumn<Customer, String> dateOfBirth;
+    @FXML
+    private TableColumn<Customer, String> driverLicenseId;
+    @FXML
+    private TableColumn<Customer, String> emailAddress;
+    @FXML
+    private TableColumn<Customer, String> firstName;
+    @FXML
+    private TableColumn<Customer, Integer> houseNumber;
+    @FXML
+    private TableColumn<Customer, String> lastName;
+    @FXML
+    private TableColumn<Customer, String> phoneNumber;
+    @FXML
+    private TableColumn<Customer, String> street;
+    @FXML
+    private TableColumn<Customer, String> city;
+    @FXML
+    private TableColumn<Customer, String> idNumber;
+    @FXML
+    private TableColumn<Customer, String> zipCode;
 
-	@Override
-	protected void handleDeleteButtonClicked() {
-		Customer toDelete = customerTableView.getSelectionModel().getSelectedItem();
-		if (null == toDelete) {
-			Alert informationDialog = GUIDialogUtil
-					.createInformationDialog(I18nComponentsUtil.getInformationDialogHeaderNoObjectSelected());
-			informationDialog.show();
-			return;
-		}
-		Alert confirmationDialog = GUIDialogUtil
-				.createConfirmationDialog(I18nComponentsUtil.getConfirmationDialogHeaderDelete());
-		Optional<ButtonType> result = confirmationDialog.showAndWait();
-		if (result.get() == ButtonType.OK) {
-			service.delete(toDelete);
-			customers.remove(toDelete);
-		}
-	}
-	
-	@Override
-	protected void handleEditButtonClicked() {
-		// TODO Auto-generated method stub
+    @FXML
+    private Button buttonCreateNewCustomer;
 
-	}
-	
-	@FXML
-	private void handleNewButtonClicked() {
-		// TODO Auto-generated method stub
+    @Override
+    protected void handleDeleteButtonClicked() {
+        Customer toDelete = customerTableView.getSelectionModel().getSelectedItem();
+        if (null == toDelete) {
+            Alert informationDialog = GUIDialogUtil.createInformationDialog(
+                    I18nComponentsUtil.getInformationDialogHeaderNoObjectSelected());
+            informationDialog.show();
+            return;
+        }
+        Alert confirmationDialog = GUIDialogUtil
+                .createConfirmationDialog(I18nComponentsUtil.getConfirmationDialogHeaderDelete());
+        Optional<ButtonType> result = confirmationDialog.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            service.delete(toDelete);
+            customers.remove(toDelete);
+        }
+    }
 
-	}
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		id.setCellValueFactory(new PropertyValueFactory<>("Id"));
-		dateOfBirth.setCellValueFactory(new PropertyValueFactory<>("DateOfBirth"));
-		driverLicenseId.setCellValueFactory(new PropertyValueFactory<>("DirverLicenseId"));
-		emailAddress.setCellValueFactory(new PropertyValueFactory<>("EmailAddress"));
-		firstName.setCellValueFactory(new PropertyValueFactory<>("FirstName"));
-		houseNumber.setCellValueFactory(new PropertyValueFactory<>("HouseNumber"));
-		lastName.setCellValueFactory(new PropertyValueFactory<>("LastName"));
-		phoneNumber.setCellValueFactory(new PropertyValueFactory<>("PhoneNumber"));
-		street.setCellValueFactory(new PropertyValueFactory<>("Street"));
-		city.setCellValueFactory(new PropertyValueFactory<>("City"));
-		idNumber.setCellValueFactory(new PropertyValueFactory<>("IdNumber"));
-		zipCode.setCellValueFactory(new PropertyValueFactory<>("ZipCode"));
-		
-		customerTableView.setItems(customers);
-	}
+    /**
+     * Handle Clicking the Edit Button.
+     *
+     */
+    @Override
+    protected void handleEditButtonClicked() {
+        Customer toEdit = customerTableView.getSelectionModel().getSelectedItem();
+        try {
+            new EditCustomerView().start(primaryStage, toEdit);
+            customerTableView.setItems(FXCollections.observableArrayList(service.findAll()));
+        } catch (Exception e) {
+            // TODO Logger?
+        }
+    }
 
-	@Override
-	protected void handleRemoveCurrentFilterButtonClicked() {
-		// TODO Auto-generated method stub
+    /**
+     * Handle Clicking the New Button.
+     */
+    @FXML
+    private void handleNewButtonClicked() {
+        try {
+            new NewCustomerView().start(primaryStage);
+            customerTableView.setItems(FXCollections.observableArrayList(service.findAll()));
+        } catch (Exception e) {
+            // TODO Logger?
+        }
+    }
 
-	}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        id.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        dateOfBirth.setCellValueFactory(new PropertyValueFactory<>("DateOfBirth"));
+        driverLicenseId.setCellValueFactory(new PropertyValueFactory<>("DirverLicenseId"));
+        emailAddress.setCellValueFactory(new PropertyValueFactory<>("EmailAddress"));
+        firstName.setCellValueFactory(new PropertyValueFactory<>("FirstName"));
+        houseNumber.setCellValueFactory(new PropertyValueFactory<>("HouseNumber"));
+        lastName.setCellValueFactory(new PropertyValueFactory<>("LastName"));
+        phoneNumber.setCellValueFactory(new PropertyValueFactory<>("PhoneNumber"));
+        street.setCellValueFactory(new PropertyValueFactory<>("Street"));
+        city.setCellValueFactory(new PropertyValueFactory<>("City"));
+        idNumber.setCellValueFactory(new PropertyValueFactory<>("IdNumber"));
+        zipCode.setCellValueFactory(new PropertyValueFactory<>("ZipCode"));
 
-	@Override
-	protected void handleApplyCurrentFilterButtonClicked() {
-		// TODO Auto-generated method stub
+        customerTableView.setItems(customers);
+    }
 
-	}
+    @Override
+    protected void handleRemoveCurrentFilterButtonClicked() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void handleApplyCurrentFilterButtonClicked() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
