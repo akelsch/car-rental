3e7ab0fd957f889eb98197d33e62aea5258b8569
Julian Quint <julianquint@gmx.net>
2018-09-02 12:10:14 +0200

    Reformat EmployeeProducer

--- src/main/java/de/htwsaar/prog3/carrental/model/producer/EmployeeProducer.java
+++ src/main/java/de/htwsaar/prog3/carrental/model/producer/EmployeeProducer.java
@@ -13,13 +13,13 @@ public class EmployeeProducer {
     private static void produce() {
         List<Employee> list = new ArrayList<>();
 
-        list.add(new Employee("Elon", "Musk","CEO"));
-        list.add(new Employee("Bill", "Gates","Customer Advisor"));
-        list.add(new Employee("Jordan", "Belfort","Customer Advisor"));
-        list.add(new Employee("Peter", "Parker","Customer Advisor"));
-        list.add(new Employee("Linus", "Torvalds","Customer Advisor"));
-        list.add(new Employee("Neil", "Armstrong","Customer Advisor"));
-        list.add(new Employee("Leonardo", "Da Vinci","Customer Advisor"));
+        list.add(new Employee("Elon", "Musk", "CEO"));
+        list.add(new Employee("Bill", "Gates", "Customer Advisor"));
+        list.add(new Employee("Jordan", "Belfort", "Customer Advisor"));
+        list.add(new Employee("Peter", "Parker", "Customer Advisor"));
+        list.add(new Employee("Linus", "Torvalds", "Customer Advisor"));
+        list.add(new Employee("Neil", "Armstrong", "Customer Advisor"));
+        list.add(new Employee("Leonardo", "Da Vinci", "Customer Advisor"));
 
         for (Employee employee : list) {
             employeeService.persist(employee);
