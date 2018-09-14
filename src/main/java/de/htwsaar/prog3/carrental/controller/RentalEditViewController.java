0d1628a969fbe29003b522a8f1dbbf0421b12308
Hagen Schackmann <Hagen.Schackmann@web.de>
2018-09-14 22:25:52 +0200

    added onActions in .fxml + deleted TextFields

--- src/main/java/de/htwsaar/prog3/carrental/controller/RentalEditViewController.java
+++ src/main/java/de/htwsaar/prog3/carrental/controller/RentalEditViewController.java
@@ -5,32 +5,37 @@ import de.htwsaar.prog3.carrental.service.CarService;
 import de.htwsaar.prog3.carrental.service.CustomerService;
 import de.htwsaar.prog3.carrental.service.EmployeeService;
 import de.htwsaar.prog3.carrental.service.RentalService;
+import de.htwsaar.prog3.carrental.util.DialogUtil;
+import de.htwsaar.prog3.carrental.util.i18n.I18nComponentsUtil;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.util.Optional;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * This is the Controller for the "Rent out Car View" of the Carrental Application.
+ * This is the Controller for the "Rental Edit View" of the Carrental Application.
  *
- * @author Michael Bös
+ * @author Hagen Schackmann, Michael Bös
  */
 
 public class RentalEditViewController {
 
     private CarService carService = new CarService();
-
     private CustomerService customerService = new CustomerService();
-
     private EmployeeService employeeService = new EmployeeService();
-
     private RentalService rentalService = new RentalService();
 
     private Car car;
+
+    private Stage modalStage;
+
 
     @FXML
     private Label CarLable;
@@ -46,39 +51,6 @@ public class RentalEditViewController {
 
     @FXML
     private DatePicker EndDatePicker;
-
-    @FXML
-    private TextField IDCardTextField;
-
-    @FXML
-    private TextField PostcodeTextField;
-
-    @FXML
-    private TextField DriverlicenseIDTextField;
-
-    @FXML
-    private TextField DateOfBirthTextField;
-
-    @FXML
-    private TextField PhoneNumberTextField;
-
-    @FXML
-    private TextField LastNameTextField;
-
-    @FXML
-    private TextField StreetTextField;
-
-    @FXML
-    private TextField HouseNumberTextField;
-
-    @FXML
-    private TextField FirstNameTextField;
-
-    @FXML
-    private TextField EMailTextField;
-
-    @FXML
-    private TextField CityTextField;
 
     @FXML
     private Button SearchForCustomerButton;
@@ -101,6 +73,20 @@ public class RentalEditViewController {
     }
 
     public void handleSearchCustomerButtonClicked() {
+
+    }
+
+    public void handleCancelButtonClicked() {
+        Alert confirmationDialog =
+                DialogUtil.createConfirmationDialog(I18nComponentsUtil.getDialogCancelConfirmationText());
+
+        Optional<ButtonType> result = confirmationDialog.showAndWait();
+        if (result.orElse(null) == ButtonType.OK) {
+            modalStage.close();
+        }
+    }
+
+    public void handleConfirmRentButtonClicked() {
 
     }
 }
