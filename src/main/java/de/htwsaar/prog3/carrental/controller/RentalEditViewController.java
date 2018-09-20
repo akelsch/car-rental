8bd8cd9963e6ad6ac2e133d7d1fad985de7899de
Hagen Schackmann <Hagen.Schackmann@web.de>
2018-09-20 20:25:31 +0200

    added Search-function + fill RentalEditView, ....

--- src/main/java/de/htwsaar/prog3/carrental/controller/RentalEditViewController.java
+++ src/main/java/de/htwsaar/prog3/carrental/controller/RentalEditViewController.java
@@ -3,8 +3,12 @@ package de.htwsaar.prog3.carrental.controller;
 import de.htwsaar.prog3.carrental.model.Car;
 import de.htwsaar.prog3.carrental.model.Customer;
 import de.htwsaar.prog3.carrental.model.Rental;
+import de.htwsaar.prog3.carrental.service.CustomerService;
+import de.htwsaar.prog3.carrental.service.GenericService;
 import de.htwsaar.prog3.carrental.util.DialogUtil;
 import de.htwsaar.prog3.carrental.util.i18n.I18nComponentsUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
@@ -23,6 +27,13 @@ import java.util.Optional;
  */
 
 public class RentalEditViewController {
+
+    private GenericService<Customer> customerservice;
+
+    private ObservableList<Customer> entities;
+
+    private Customer customer;
+
     private Rental rental;
 
     @Setter
@@ -91,40 +102,31 @@ public class RentalEditViewController {
     @FXML
     private TextArea noteTextArea;
 
+
     public void initialize(Rental rental) {
         this.rental = rental;
 
         beginDatePicker.setValue(getDatePickerConverter().fromString(rental.getBegin()));
         endDatePicker.setValue(getDatePickerConverter().fromString(rental.getEnd()));
         noteTextArea.setText(rental.getNote());
+        driverLicenseIdTextField.requestFocus();
 
         Car car = rental.getCar();
         if (car != null) {
             carLabel.setText(car.toString());
         }
 
-        Customer customer = rental.getCustomer();
-        if (customer != null) {
-            driverLicenseIdTextField.setText(customer.getDriverLicenseId());
-            firstNameTextField.setText(customer.getFirstName());
-            lastNameTextField.setText(customer.getLastName());
-            idNumberTextField.setText(customer.getIdNumber());
-            dateOfBirthTextField.setText(customer.getDateOfBirth());
-            zipCodeTextField.setText(Integer.toString(customer.getZipCode()));
-            cityTextField.setText(customer.getCity());
-            streetTextField.setText(customer.getStreet());
-            houseNumberTextField.setText(customer.getHouseNumber());
-            emailTextField.setText(customer.getEmailAddress());
-            phoneNumberTextField.setText(customer.getPhoneNumber());
-        }
-
-        driverLicenseIdTextField.requestFocus();
+        customer = rental.getCustomer();
+        fillCustomerFields();
     }
 
     public void handleConfirmButtonClicked() {
     }
 
     public void handleSearchButtonClicked() {
+
+        customer = findByDriverID(driverLicenseIdTextField.getText());
+        fillCustomerFields();
     }
 
     public void handleCancelButtonClicked() {
@@ -138,9 +140,15 @@ public class RentalEditViewController {
     }
 
     public void handleApplyButtonClicked() {
-        // rental.setCar(car);
-        // rental.setCustomer(customer);
-        // rental.setEmployee(employee);
+      rental.setBegin(getDatePickerConverter().toString(beginDatePicker.getValue()));
+      rental.setEnd(getDatePickerConverter().toString(endDatePicker.getValue()));
+     // rental.setEmployee();
+      rental.setCustomer(findByDriverID(driverLicenseIdTextField.getText()));
+      rental.setExtraCosts(Integer.parseInt(dailyPriceTextField.getText()));
+      rental.setNote(noteTextArea.getText());
+
+      applyClicked = true;
+      modalStage.close();
     }
 
     private StringConverter<LocalDate> getDatePickerConverter() {
@@ -167,13 +175,107 @@ public class RentalEditViewController {
                 }
             }
         };
-
         beginDatePicker.setConverter(converter);
         beginDatePicker.setPromptText(pattern);
-
         endDatePicker.setConverter(converter);
         endDatePicker.setPromptText(pattern);
-
         return converter;
     }
+
+
+    private Customer findByDriverID(String driverID) {
+        customerservice = new CustomerService();
+        entities = FXCollections.observableArrayList(customerservice.findAll());
+        Customer correctCustomer;
+        int i;
+
+        for(i = 0; i < entities.size(); i++) {
+            if ((entities.get(i).getDriverLicenseId()).contentEquals(driverID)) {
+                correctCustomer = entities.get(i);
+                System.out.println("FoundFoundFound: " + entities.get(i).getFirstName());
+                return correctCustomer;
+            }
+        } return null;
+    }
+
+
+    private void fillCustomerFields() {
+        if (customer != null) {
+            driverLicenseIdTextField.setText(customer.getDriverLicenseId());
+            firstNameTextField.setText(customer.getFirstName());
+            lastNameTextField.setText(customer.getLastName());
+            idNumberTextField.setText(customer.getIdNumber());
+            dateOfBirthTextField.setText(customer.getDateOfBirth());
+            zipCodeTextField.setText(Integer.toString(customer.getZipCode()));
+            cityTextField.setText(customer.getCity());
+            streetTextField.setText(customer.getStreet());
+            houseNumberTextField.setText(customer.getHouseNumber());
+            emailTextField.setText(customer.getEmailAddress());
+            phoneNumberTextField.setText(customer.getPhoneNumber());
+        }
+    }
+
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        if (driverLicenseIdTextField.getText() == null || driverLicenseIdTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidDriverLicenseId() + "\n";
+        }
+
+        if (firstNameTextField.getText() == null || firstNameTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidFirstName() + "\n";
+        }
+
+        if (lastNameTextField.getText() == null || lastNameTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidLastName() + "\n";
+        }
+
+        if (idNumberTextField.getText() == null || idNumberTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidIdNumber() + "\n";
+        }
+
+        if (dateOfBirthTextField.getText() == null
+                || dateOfBirthTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidDateOfBirth() + "\n";
+        }
+
+        if (zipCodeTextField.getText() == null
+                || zipCodeTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidZipCode() + "\n";
+        }
+
+        if (cityTextField.getText() == null || cityTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidCityName() + "\n";
+        }
+
+        if (streetTextField.getText() == null || streetTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidStreetName() + "\n";
+        }
+
+        if (houseNumberTextField.getText() == null || houseNumberTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidHouseNumber() + "\n";
+        }
+
+        if (emailTextField.getText() == null || emailTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidEmailAddress() + "\n";
+        }
+
+        if (phoneNumberTextField.getText() == null || phoneNumberTextField.getText().length() == 0) {
+            errorMessage += I18nComponentsUtil.getRentalNoValidPhoneNumber() + "\n";
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(modalStage);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+
+            return false;
+        }
+    }
 }
+
