2d906df8f461e0db9f7449bc8c0f84cc1cdcad6c
Lukas Raubuch <lukas.raubuch@web.de>
2021-01-18 22:14:21 +0100

    Implemented contains comparision for all employee fields.

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/EmployeeTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/EmployeeTableViewControllerSearchIT.java
@@ -30,6 +30,7 @@ import javafx.scene.control.TextField;
 class EmployeeTableViewControllerSearchIT {
 	private static final int EQUAL_OPERATOR_COMBOBOX = 0;
 	private static final int NOT_EQUAL_OPERATOR_COMBOBOX = 1;
+	private static final int CONTAINS_OPERATOR_COMBOBOX = 2;
 	
 	private static final int ID_ATTRIBUTE_COMBOBOX = 0;
 	private static final int FIRST_NAME_ATTRIBUTE_COMBOBOX = 1;
@@ -115,7 +116,6 @@ class EmployeeTableViewControllerSearchIT {
         robot.clickOn(searchButton);
 
         assertTrue(table.getItems().size() < beforeSize);
-        assertEquals(1, table.getItems().size());
         assertTrue(table.getItems().contains(knownEmployee));
     }
     
@@ -142,7 +142,6 @@ class EmployeeTableViewControllerSearchIT {
         robot.clickOn(searchButton);
 
         assertTrue(table.getItems().size() < beforeSize);
-        assertEquals(1, table.getItems().size());
         assertTrue(table.getItems().contains(knownEmployee));
     }
     
@@ -274,5 +273,83 @@ class EmployeeTableViewControllerSearchIT {
 
         assertTrue(table.getItems().size() < beforeSize);
         assertFalse(table.getItems().contains(knownEmployee));
+    }
+    
+    @Test
+    void testFirstNameContains(FxRobot robot) {
+        TableView<Employee> table = robot.lookup("#entityTable").query();
+        int beforeSize = table.getItems().size();
+
+        // Attribute
+        ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+        robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(FIRST_NAME_ATTRIBUTE_COMBOBOX));
+
+        // Operator
+        ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+        robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(CONTAINS_OPERATOR_COMBOBOX));
+
+        // Search string
+        TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+        robot.clickOn(searchValueTextField);
+        robot.write("jör");
+
+        // Search via button
+        Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+        robot.clickOn(searchButton);
+
+        assertTrue(table.getItems().size() < beforeSize);
+        assertTrue(table.getItems().contains(knownEmployee));
+    }
+    
+    @Test
+    void testLastNameContains(FxRobot robot) {
+        TableView<Employee> table = robot.lookup("#entityTable").query();
+        int beforeSize = table.getItems().size();
+
+        // Attribute
+        ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+        robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(LAST_NAME_ATTRIBUTE_COMBOBOX));
+
+        // Operator
+        ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+        robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(CONTAINS_OPERATOR_COMBOBOX));
+
+        // Search string
+        TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+        robot.clickOn(searchValueTextField);
+        robot.write("chere");
+
+        // Search via button
+        Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+        robot.clickOn(searchButton);
+
+        assertTrue(table.getItems().size() < beforeSize);
+        assertTrue(table.getItems().contains(knownEmployee));
+    }
+    
+    @Test
+    void testPositionContains(FxRobot robot) {
+        TableView<Employee> table = robot.lookup("#entityTable").query();
+        int beforeSize = table.getItems().size();
+
+        // Attribute
+        ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+        robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(POSITION_ATTRIBUTE_COMBOBOX));
+
+        // Operator
+        ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+        robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(CONTAINS_OPERATOR_COMBOBOX));
+
+        // Search string
+        TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+        robot.clickOn(searchValueTextField);
+        robot.write("nager");
+
+        // Search via button
+        Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+        robot.clickOn(searchButton);
+
+        assertTrue(table.getItems().size() < beforeSize);
+        assertTrue(table.getItems().contains(knownEmployee));
     }
 }
