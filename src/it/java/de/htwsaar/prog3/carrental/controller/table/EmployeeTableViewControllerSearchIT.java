fe26d283d72dab6c6370ecda3a08bd2ff24ba58d
Arthur Kelsch <akelsch@users.noreply.github.com>
2021-01-20 13:31:22 +0100

    IntelliJ formatting & warnings

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/EmployeeTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/EmployeeTableViewControllerSearchIT.java
@@ -1,7 +1,14 @@
 package de.htwsaar.prog3.carrental.controller.table;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import de.htwsaar.prog3.carrental.TestUiApplication;
+import de.htwsaar.prog3.carrental.application.StageInitializer;
+import de.htwsaar.prog3.carrental.model.Employee;
+import de.htwsaar.prog3.carrental.repository.EmployeeRepository;
+import de.htwsaar.prog3.carrental.util.filter.Operator;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,44 +22,37 @@ import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.util.WaitForAsyncUtils;
 
-import de.htwsaar.prog3.carrental.TestUiApplication;
-import de.htwsaar.prog3.carrental.application.StageInitializer;
-import de.htwsaar.prog3.carrental.model.Employee;
-import de.htwsaar.prog3.carrental.repository.EmployeeRepository;
-import de.htwsaar.prog3.carrental.util.filter.Operator;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(ApplicationExtension.class)
 class EmployeeTableViewControllerSearchIT {
-	private static final int EQUAL_OPERATOR_COMBOBOX = 0;
-	private static final int NOT_EQUAL_OPERATOR_COMBOBOX = 1;
-	private static final int CONTAINS_OPERATOR_COMBOBOX = 2;
-	private static final int GREATER_OPERATOR_COMBOBOX = 3;
-	private static final int LESS_OPERATOR_COMBOBOX = 4;
-	
-	private static final int ID_ATTRIBUTE_COMBOBOX = 0;
-	private static final int FIRST_NAME_ATTRIBUTE_COMBOBOX = 1;
-	private static final int LAST_NAME_ATTRIBUTE_COMBOBOX = 2;
-	private static final int POSITION_ATTRIBUTE_COMBOBOX = 3;
 
-	private static Employee knownEmployee;
-	
-	@Autowired
-	private StageInitializer stageInitializer;
+    private static final int EQUAL_OPERATOR_COMBOBOX = 0;
+    private static final int NOT_EQUAL_OPERATOR_COMBOBOX = 1;
+    private static final int CONTAINS_OPERATOR_COMBOBOX = 2;
+    private static final int GREATER_OPERATOR_COMBOBOX = 3;
+    private static final int LESS_OPERATOR_COMBOBOX = 4;
 
-	@Autowired
+    private static final int ID_ATTRIBUTE_COMBOBOX = 0;
+    private static final int FIRST_NAME_ATTRIBUTE_COMBOBOX = 1;
+    private static final int LAST_NAME_ATTRIBUTE_COMBOBOX = 2;
+    private static final int POSITION_ATTRIBUTE_COMBOBOX = 3;
+
+    private static Employee knownEmployee;
+
+    @Autowired
+    private StageInitializer stageInitializer;
+
+    @Autowired
     private ConfigurableApplicationContext applicationContext;
 
     @BeforeAll
     static void beforeAll(@Autowired EmployeeRepository employeeRepository) {
         knownEmployee = employeeRepository.save(Employee.builder()
-        		.firstName("Björn")
-        		.lastName("Scherer")
-        		.position("Manager")
+                .firstName("Björn")
+                .lastName("Scherer")
+                .position("Manager")
                 .build());
     }
 
@@ -67,7 +67,7 @@ class EmployeeTableViewControllerSearchIT {
     void tearDown() throws Exception {
         FxToolkit.cleanupStages();
     }
-	
+
     @Test
     void testIdEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -94,7 +94,7 @@ class EmployeeTableViewControllerSearchIT {
         assertEquals(1, table.getItems().size());
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testFirstNameEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -111,7 +111,7 @@ class EmployeeTableViewControllerSearchIT {
         // Search string
         TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
         robot.clickOn(searchValueTextField);
-        robot.write(knownEmployee.getFirstName().toString());
+        robot.write(knownEmployee.getFirstName());
 
         // Search via button
         Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
@@ -120,7 +120,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testLastNameEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -137,7 +137,7 @@ class EmployeeTableViewControllerSearchIT {
         // Search string
         TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
         robot.clickOn(searchValueTextField);
-        robot.write(knownEmployee.getLastName().toString());
+        robot.write(knownEmployee.getLastName());
 
         // Search via button
         Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
@@ -146,7 +146,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testPositionEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -163,7 +163,7 @@ class EmployeeTableViewControllerSearchIT {
         // Search string
         TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
         robot.clickOn(searchValueTextField);
-        robot.write(knownEmployee.getPosition().toString());
+        robot.write(knownEmployee.getPosition());
 
         // Search via button
         Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
@@ -172,7 +172,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testIdNotEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -198,7 +198,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertFalse(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testFirstNameNotEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -215,7 +215,7 @@ class EmployeeTableViewControllerSearchIT {
         // Search string
         TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
         robot.clickOn(searchValueTextField);
-        robot.write(knownEmployee.getFirstName().toString());
+        robot.write(knownEmployee.getFirstName());
 
         // Search via button
         Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
@@ -224,7 +224,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertFalse(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testLastNameNotEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -241,7 +241,7 @@ class EmployeeTableViewControllerSearchIT {
         // Search string
         TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
         robot.clickOn(searchValueTextField);
-        robot.write(knownEmployee.getLastName().toString());
+        robot.write(knownEmployee.getLastName());
 
         // Search via button
         Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
@@ -250,7 +250,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertFalse(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testPositionNotEqual(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -267,7 +267,7 @@ class EmployeeTableViewControllerSearchIT {
         // Search string
         TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
         robot.clickOn(searchValueTextField);
-        robot.write(knownEmployee.getPosition().toString());
+        robot.write(knownEmployee.getPosition());
 
         // Search via button
         Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
@@ -276,7 +276,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertFalse(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testFirstNameContains(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -302,7 +302,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testLastNameContains(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -328,7 +328,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testPositionContains(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -354,7 +354,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testIdGreater(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
@@ -380,7 +380,7 @@ class EmployeeTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownEmployee));
     }
-    
+
     @Test
     void testIdLess(FxRobot robot) {
         TableView<Employee> table = robot.lookup("#entityTable").query();
