252841149a54c39c400fad494a823f648e165143
Julian Quint <julianquint@gmx.net>
2021-02-01 23:55:17 +0100

    Add tests for RentalTableView

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerIT.java
@@ -3,13 +3,16 @@ package de.htwsaar.prog3.carrental.controller.table;
 import de.htwsaar.prog3.carrental.TestUiApplication;
 import de.htwsaar.prog3.carrental.application.StageInitializer;
 import de.htwsaar.prog3.carrental.model.Rental;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import de.htwsaar.prog3.carrental.util.fx.IntegerField;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,11 +45,31 @@ class RentalTableViewControllerIT {
     }
 
     @AfterEach
-    void tearDown() throws Exception {
-        FxToolkit.cleanupStages();
+    void tearDown() {
     }
 
     @Test
+    @Order(1)
+    void handleEditClicked(FxRobot robot) {
+        BorderPane rentalTableView = (BorderPane) robot.window(0).getScene().getRoot();
+        TableView<Rental> table = (TableView<Rental>) rentalTableView.getCenter();
+        table.getSelectionModel().selectLast();
+
+        HBox bottom = (HBox) rentalTableView.getBottom();
+        ObservableList<Node> buttons = bottom.getChildrenUnmodifiable();
+        Button editButton = robot.from(buttons.get(0)).queryButton();
+        robot.clickOn(editButton);
+
+        IntegerField extraCostsIntegerField = robot.lookup("#extraCostsIntegerField").queryAs(IntegerField.class);
+        robot.doubleClickOn(extraCostsIntegerField).write("1337");
+        robot.press(KeyCode.ENTER);
+
+        Rental rental = table.getSelectionModel().getSelectedItem();
+        assertEquals(1337, rental.getExtraCosts());
+    }
+
+    @Test
+    @Order(2)
     void testRegularDelete(FxRobot robot) {
         TableView<Rental> table = robot.lookup("#entityTable").query();
         int beforeSize = table.getItems().size();
@@ -61,6 +84,7 @@ class RentalTableViewControllerIT {
     }
 
     @Test
+    @Order(3)
     void testActiveDelete(FxRobot robot) {
         TableView<Rental> table = robot.lookup("#entityTable").query();
         int beforeSize = table.getItems().size();
