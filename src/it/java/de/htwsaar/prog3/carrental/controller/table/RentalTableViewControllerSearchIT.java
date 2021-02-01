48d5a3e9090677a88985e925c916ebde374b0169
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 14:12:55 +0100

    Implemented Rental Equals test for employee

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
@@ -207,4 +207,24 @@ class RentalTableViewControllerSearchIT {
 
 		assertTrue(table.getItems().contains(knownRental));
 	}
+	
+	@Test
+	void testEmployeeEqual(FxRobot robot) {
+		TableView<Rental> table = robot.lookup("#entityTable").query();
+		
+		ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+		robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(EMPLOYEE_ATTRIBUTE_COMBOBOX)); 
+		
+		ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+		robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(EQUAL_OPERATOR_COMBOBOX));
+
+		TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+		robot.clickOn(searchValueTextField);
+		robot.write(knownRental.getEmployee().toString());
+
+		Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+		robot.clickOn(searchButton);
+
+		assertTrue(table.getItems().contains(knownRental));
+	}
 }
