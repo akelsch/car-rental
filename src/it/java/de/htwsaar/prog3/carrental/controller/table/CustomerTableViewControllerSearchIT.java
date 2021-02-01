3c425cbef231fb5d8f9c11aa6e06da5008b9be40
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 10:29:21 +0100

    Created a file to test all CustomerTableViewFields and implemented equals test for ID

---
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/CustomerTableViewControllerSearchIT.java
@@ -0,0 +1,106 @@
+package de.htwsaar.prog3.carrental.controller.table;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.util.WaitForAsyncUtils;
+
+import de.htwsaar.prog3.carrental.TestUiApplication;
+import de.htwsaar.prog3.carrental.application.StageInitializer;
+import de.htwsaar.prog3.carrental.model.Customer;
+import de.htwsaar.prog3.carrental.repository.CustomerRepository;
+import de.htwsaar.prog3.carrental.util.filter.Operator;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+@SpringBootTest
+@ExtendWith(ApplicationExtension.class)
+class CustomerTableViewControllerSearchIT {
+	private static final int EQUAL_OPERATOR_COMBOBOX = 0;
+    private static final int NOT_EQUAL_OPERATOR_COMBOBOX = 1;
+    private static final int CONTAINS_OPERATOR_COMBOBOX = 2;
+    private static final int GREATER_OPERATOR_COMBOBOX = 3;
+    private static final int LESS_OPERATOR_COMBOBOX = 4;
+
+    private static final int ID_ATTRIBUTE_COMBOBOX = 0;
+    private static final int FIRST_NAME_ATTRIBUTE_COMBOBOX = 1;
+    private static final int LAST_NAME_ATTRIBUTE_COMBOBOX = 2;
+    private static final int POSITION_ATTRIBUTE_COMBOBOX = 3;
+
+    private static Customer knownCustomer;
+
+    @Autowired
+    private StageInitializer stageInitializer;
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeAll
+    static void beforeAll(@Autowired CustomerRepository customerRepository) {
+        knownCustomer = customerRepository.save(Customer.builder()
+                .firstName("Bill")
+                .lastName("Gates")
+                .street("Microsoft, Ave")
+                .zipcode(54321)
+                .city("Redmond")
+                .phone("+12345678900")
+                .email("billyg@microsoft.com")
+                .dateOfBirth(LocalDate.parse("1955-10-28"))
+                .idNumber("ABC123456")
+                .driverLicenseNumber("ABCDEFG1234")
+                .build());
+    }
+    
+    @BeforeEach
+    void setUp() throws Exception {
+        FxToolkit.setupApplication(() -> new TestUiApplication(applicationContext));
+        stageInitializer.switchToCustomerTableView();
+        WaitForAsyncUtils.waitForFxEvents();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        FxToolkit.cleanupStages();
+    }
+    
+    @Test
+    void testIdEqual(FxRobot robot) {
+        TableView<Customer> table = robot.lookup("#entityTable").query();
+        int beforeSize = table.getItems().size();
+
+        // Attribute
+        ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+        robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(ID_ATTRIBUTE_COMBOBOX));
+
+        // Operator
+        ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+        robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(EQUAL_OPERATOR_COMBOBOX));
+
+        // Search string
+        TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+        robot.clickOn(searchValueTextField);
+        robot.write(knownCustomer.getId().toString());
+
+        // Search via button
+        Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+        robot.clickOn(searchButton);
+
+        assertTrue(table.getItems().size() < beforeSize);
+        assertEquals(1, table.getItems().size());
+        assertTrue(table.getItems().contains(knownCustomer));
+    }
+}
