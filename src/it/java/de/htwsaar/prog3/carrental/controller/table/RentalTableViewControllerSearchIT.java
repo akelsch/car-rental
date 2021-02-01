130312ee61c4c4eec049a1b053ef5ea889685aaf
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 14:08:46 +0100

    Implemented Rental Equals test for car

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
@@ -167,4 +167,24 @@ class RentalTableViewControllerSearchIT {
 
 		assertTrue(table.getItems().contains(knownRental));
 	}
+	
+	@Test
+	void testCarEqual(FxRobot robot) {
+		TableView<Rental> table = robot.lookup("#entityTable").query();
+		
+		ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+		robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(CAR_ATTRIBUTE_COMBOBOX)); 
+		
+		ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+		robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(EQUAL_OPERATOR_COMBOBOX));
+
+		TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+		robot.clickOn(searchValueTextField);
+		robot.write(knownRental.getCar().toString());
+
+		Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+		robot.clickOn(searchButton);
+
+		assertTrue(table.getItems().contains(knownRental));
+	}
 }
