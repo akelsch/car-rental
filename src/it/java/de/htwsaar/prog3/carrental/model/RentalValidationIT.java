09bbc28eacb84ad280fd3d186dae272ae9064b57
Julian Quint <julianquint@gmx.net>
2021-02-01 16:15:03 +0100

    Fix code smell

--- src/it/java/de/htwsaar/prog3/carrental/model/RentalValidationIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/model/RentalValidationIT.java
@@ -16,7 +16,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class RentalValidationIT {
+class RentalValidationIT {
 
     @Autowired
     private Validator validator;
