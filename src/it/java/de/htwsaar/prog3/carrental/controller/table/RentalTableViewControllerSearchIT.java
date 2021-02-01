18dd40bb4d2fdce2094e0ca30ed5bac6975dace7
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 14:26:14 +0100

    Implemented Rental Not Equals test for end

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
@@ -317,4 +317,24 @@ class RentalTableViewControllerSearchIT {
 
 		assertFalse(table.getItems().contains(knownRental));
 	}
+	
+	@Test
+	void testEndNotEqual(FxRobot robot) {
+		TableView<Rental> table = robot.lookup("#entityTable").query();	
+		
+		ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+		robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(END_ATTRIBUTE_COMBOBOX)); 
+		
+		ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+		robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(NOT_EQUAL_OPERATOR_COMBOBOX));
+
+		TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+		robot.clickOn(searchValueTextField);
+		robot.write(knownRental.getEnd().toString());
+
+		Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+		robot.clickOn(searchButton);
+
+		assertFalse(table.getItems().contains(knownRental));
+	}
 }
