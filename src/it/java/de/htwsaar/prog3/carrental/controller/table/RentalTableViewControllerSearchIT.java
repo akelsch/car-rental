7db29b9252a4b9db9d1a321760de29b6b58d1700
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 14:22:57 +0100

    Implemented Rental Not Equals test for id

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
@@ -271,4 +271,30 @@ class RentalTableViewControllerSearchIT {
 
 		assertTrue(table.getItems().contains(knownRental));
 	}
+	
+	//////////////////////////////
+	// NOT EQUAL TESTS TO FOLLOW /
+	//////////////////////////////
+	
+	@Test
+	void testIdNotEqual(FxRobot robot) {
+		TableView<Rental> table = robot.lookup("#entityTable").query();
+		int beforeSize = table.getItems().size();		
+		
+		ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+		robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(ID_ATTRIBUTE_COMBOBOX)); 
+		
+		ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+		robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(NOT_EQUAL_OPERATOR_COMBOBOX));
+
+		TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+		robot.clickOn(searchValueTextField);
+		robot.write(knownRental.getId().toString());
+
+		Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+		robot.clickOn(searchButton);
+
+		assertTrue(table.getItems().size() < beforeSize);
+		assertFalse(table.getItems().contains(knownRental));
+	}
 }
