41c7e53c67045eb4281307ee5927dd08bd128cd3
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 14:31:02 +0100

    Implemented Rental Not Equals test for employee

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
@@ -377,4 +377,24 @@ class RentalTableViewControllerSearchIT {
 
 		assertFalse(table.getItems().contains(knownRental));
 	}
+	
+	@Test
+	void testEmployeeNotEqual(FxRobot robot) {
+		TableView<Rental> table = robot.lookup("#entityTable").query();	
+		
+		ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+		robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(EMPLOYEE_ATTRIBUTE_COMBOBOX)); 
+		
+		ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+		robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(NOT_EQUAL_OPERATOR_COMBOBOX));
+
+		TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+		robot.clickOn(searchValueTextField);
+		robot.write(knownRental.getEmployee().toString());
+
+		Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+		robot.clickOn(searchButton);
+
+		assertFalse(table.getItems().contains(knownRental));
+	}
 }
