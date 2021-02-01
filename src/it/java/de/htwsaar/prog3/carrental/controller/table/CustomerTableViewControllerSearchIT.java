466223769142f62537efadf553369aeb5e1bc7ee
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 11:49:59 +0100

    Implemented Customer Greater test for date of birth

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/CustomerTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/CustomerTableViewControllerSearchIT.java
@@ -862,4 +862,31 @@ class CustomerTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertTrue(table.getItems().contains(knownCustomer));
     }
+    
+    @Test
+    void testDateOfBirthGreater(FxRobot robot) {
+        TableView<Customer> table = robot.lookup("#entityTable").query();
+        int beforeSize = table.getItems().size();
+
+        // Attribute
+        ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+        robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(DATE_OF_BIRTH_ATTRIBUTE_COMBOBOX));
+
+        // Operator
+        ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+        robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(GREATER_OPERATOR_COMBOBOX));
+
+        // Search string
+        TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+        robot.clickOn(searchValueTextField);
+        robot.write(String.valueOf(DATE_OF_BIRTH_GREATER_TEST));
+
+        // Search via button
+        Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+        robot.clickOn(searchButton);
+
+        assertTrue(table.getItems().size() < beforeSize);
+        assertFalse(table.getItems().contains(knownCustomer));
+    }
+
 }
