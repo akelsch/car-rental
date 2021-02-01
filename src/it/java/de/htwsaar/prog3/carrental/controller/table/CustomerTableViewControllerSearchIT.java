06af0de4537d5053543b4d64f1e9a2ac5b7fe831
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 11:56:12 +0100

    Implemented Customer Less test for zipcode

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/CustomerTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/CustomerTableViewControllerSearchIT.java
@@ -916,4 +916,28 @@ class CustomerTableViewControllerSearchIT {
 
         assertTrue(table.getItems().contains(knownCustomer));
     }
+    
+    @Test
+    void testZipcodeLess(FxRobot robot) {
+        TableView<Customer> table = robot.lookup("#entityTable").query();
+
+        // Attribute
+        ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+        robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(ZIPCODE_ATTRIBUTE_COMBOBOX));
+
+        // Operator
+        ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+        robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(LESS_OPERATOR_COMBOBOX));
+
+        // Search string
+        TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+        robot.clickOn(searchValueTextField);
+        robot.write(String.valueOf(ZIPCODE_LESS_TEST));
+
+        // Search via button
+        Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+        robot.clickOn(searchButton);
+
+        assertTrue(table.getItems().contains(knownCustomer));
+    }
 }
