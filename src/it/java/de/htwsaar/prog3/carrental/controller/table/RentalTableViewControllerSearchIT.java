c44b5976c3a20cdf14483bb031352d9708195d97
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 14:34:12 +0100

    Implemented Rental Not Equals test for note

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/RentalTableViewControllerSearchIT.java
@@ -417,4 +417,24 @@ class RentalTableViewControllerSearchIT {
 
 		assertFalse(table.getItems().contains(knownRental));
 	}
+	
+	@Test
+	void testNoteNotEqual(FxRobot robot) {
+		TableView<Rental> table = robot.lookup("#entityTable").query();	
+		
+		ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+		robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(NOTE_ATTRIBUTE_COMBOBOX)); 
+		
+		ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+		robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(NOT_EQUAL_OPERATOR_COMBOBOX));
+
+		TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+		robot.clickOn(searchValueTextField);
+		robot.write(knownRental.getNote());
+
+		Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+		robot.clickOn(searchButton);
+
+		assertFalse(table.getItems().contains(knownRental));
+	}
 }
