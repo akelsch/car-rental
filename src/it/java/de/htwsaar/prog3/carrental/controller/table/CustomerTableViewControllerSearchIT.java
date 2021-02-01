bdcf42cce9790fbf5bca195f71cb020c6cd202e1
Lukas Raubuch <lukas.raubuch@web.de>
2021-02-01 11:15:07 +0100

    Implemented Customer Not Equals test for zipcode

--- src/it/java/de/htwsaar/prog3/carrental/controller/table/CustomerTableViewControllerSearchIT.java
+++ src/it/java/de/htwsaar/prog3/carrental/controller/table/CustomerTableViewControllerSearchIT.java
@@ -480,4 +480,30 @@ class CustomerTableViewControllerSearchIT {
         assertTrue(table.getItems().size() < beforeSize);
         assertFalse(table.getItems().contains(knownCustomer));
     }
+    
+    @Test
+    void testZipcodeNotEqual(FxRobot robot) {
+        TableView<Customer> table = robot.lookup("#entityTable").query();
+        int beforeSize = table.getItems().size();
+
+        // Attribute
+        ComboBox<String> searchAttributeComboBox = robot.lookup("#searchAttributeComboBox").query();
+        robot.interact(() -> searchAttributeComboBox.getSelectionModel().select(ZIPCODE_ATTRIBUTE_COMBOBOX));
+
+        // Operator
+        ComboBox<Operator> searchOperatorComboBox = robot.lookup("#searchOperatorComboBox").query();
+        robot.interact(() -> searchOperatorComboBox.getSelectionModel().select(NOT_EQUAL_OPERATOR_COMBOBOX));
+
+        // Search string
+        TextField searchValueTextField = robot.lookup("#searchValueTextField").query();
+        robot.clickOn(searchValueTextField);
+        robot.write("" + knownCustomer.getZipcode());
+
+        // Search via button
+        Button searchButton = robot.from(searchAttributeComboBox.getParent().getChildrenUnmodifiable()).nth(3).queryButton();
+        robot.clickOn(searchButton);
+
+        assertTrue(table.getItems().size() < beforeSize);
+        assertFalse(table.getItems().contains(knownCustomer));
+    }
 }
